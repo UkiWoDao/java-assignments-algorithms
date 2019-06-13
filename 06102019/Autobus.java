d7d833a195dce1397f094cf480691c447e35a537
Uros Vucenovic <u_vucenovic@yahoo.com>
2019-06-13 03:19:43 +0200

    Create Autobus.java

---
+++ 06102019/Autobus.java
@@ -0,0 +1,69 @@
+package domaci06102019;
+
+public class Autobus {
+	
+	private String naziv;
+	private Vozac vozac;
+	private double cenaKarte;
+	private Putnik putnici[];
+	
+	public Autobus(String naziv, double cena, int i) {
+		this.naziv = naziv;
+		this.cenaKarte = cena;
+		putnici = new Putnik[i];
+	}
+	
+	public void dodatiVozaca() {
+		// vozacevo mesto je naravno prvo, ili kako bi dusan rekao nulto
+		if(vozac == null) {
+			vozac = new Vozac();
+			System.out.println("Novi vozac je postavljen");
+		} else {
+			System.out.println("Vec postoji vozac");
+		}
+	}
+	
+	public void sklonitiVozaca() {
+		if(vozac == null) {
+			System.out.println("Vozaca ionako nema");
+		} else {
+			vozac = null;
+			System.out.println("Vozac sklonjen");
+		}
+	}
+	
+	public boolean imaVozac() {
+		if(vozac != null) {
+			return true;
+		} else return false;
+	}
+	
+	public void dodatiPutnika(Putnik p, int i) {
+		// provera da li parametar odgovara realnom mestu
+		if(i < 0 || i > putnici.length) {
+			System.out.println("Unesite sediste od 0 do " + putnici.length);
+			return;
+		} 
+		
+		// provera da li je autobus pun
+		if(putnici != null){
+			return;
+		} else {
+			for(int k = 1; k < putnici.length; k++) {
+				if(putnici[k] == null) {
+					putnici[k] = p;
+				}
+			}
+		}
+	}
+	
+	public void naplatitiNesrecnicima() {
+		if(this.imaVozac() == true) {
+			for(int i = 0; i < putnici.length; i++) {
+				if(putnici[i].getNovac() < this.cenaKarte) {
+					putnici[i] = null;
+				}
+			}
+		} else return;
+	}
+}
